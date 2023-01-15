38765034372a6a3387cf12589bd9d68b922f358d
Suraj8108 <suraj98693@gmail.com>
2023-01-15 17:33:33 +0530

    Added  Scan PackaGES

--- src/main/java/com/example/bookmicro/BookmicroApplication.java
+++ src/main/java/com/example/bookmicro/BookmicroApplication.java
@@ -3,11 +3,17 @@ package com.example.bookmicro;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.bookmicro.controller", 
+		"com.example.bookmicro.config", "com.example.bookmicro.dto", "com.example.bookmicro.helper"
+		,"com.example.bookmicro.service"})
+@EnableJpaRepositories({"com.example.bookmicro.dao"})
+@EntityScan(basePackages = {"com.example.bookmicro.entity"})
 public class BookmicroApplication {
 
 	public static void main(String[] args) {
